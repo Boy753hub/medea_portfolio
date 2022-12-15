1ca5d2de47af94a35eb05b623bce57a419a6ba3a
luka tskhomelidze <89034140+Boy753hub@users.noreply.github.com>
2022-12-15 18:04:24 +0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { CameraComponent } from './camera/camera.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServicesComponent } from './services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'about', component: AboutComponent},
+  { path: 'contact', component: ContactComponent},
+  { path: 'camera', component: CameraComponent},
+  { path: 'services', component: ServicesComponent},
+  { path: '**', redirectTo: 'about'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
